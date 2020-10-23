3ba28f981052ec1a33561efd536e80eb9ae4c3ba
sunxi2003 <sunxi2003@163.com>
2020-10-23 13:59:56 +0800

    1023005

--- test.java
+++ test.java
@@ -1,7 +1,4 @@
-public class hello {
-    /* 第一个Java程序
-     * 它将打印字符串 Hello World
-     */
+public class test {
     public static void main(String[] args) {
         System.out.println("Hello World"); // 打印 Hello World
     }
